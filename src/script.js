250f490f3dfc14964a100e3da904aa711bab3b5e
Josiah-090602 <viernesjosiah6@gmail.com>
2023-12-20 21:44:54 +0800

    Modified: script.js added active indicator to menu buttons

--- src/script.js
+++ src/script.js
@@ -1,15 +1,31 @@
-let menuLink = document.querySelectorAll('.menu-link')
+const header = document.getElementById('menu-section')
+const menuLink = header.getElementsByClassName('menu-btn')
 
 
-menuLink.forEach( menu => {
-    menu.addEventListener("click", _e => {
-        removeActive()
-        menu.classList.add('active')
-    })
-})
+for (let i = 0; i < menuLink.length; i++){
+    menuLink[i].addEventListener("click", function() {
+        const current = document.getElementsByClassName("active")
+        current[0].className = current[0].className.replace("active","")
+        this.className += " active"
+    })    
+}
 
-const removeActive = () => {
-    menuLink.forEach( menu => {
-        menu.classList.remove('active')
-    })
+
+const menus = [document.querySelectorAll('.menu-btn')]
+const dataContent = []
+dataContent[0] = "Bulaklak"
+dataContent[1] = "Sinigang"
+dataContent[2] = "Halo-halo"
+
+console.log (menus)
+
+function changeContent () {
+    if (menuLink.classList.contain){
+        console.log (dataContent[0])
+    }else if(menus[1] = "button.menu-btn.active"){
+        console.log (dataContent[1])
+    }else if(menus[2] = "button.menu-btn.active"){
+        console.log (dataContent[2])
+    }
+    
 }
